515c2e2e96c8bc318df98f9d87872baa9aa0b720
thecrimsonthreat <hayesraven@gmail.com>
2021-05-19 10:53:38 -0400

    Update temperature.cpp

--- codecademy/c++/temperature.cpp
+++ codecademy/c++/temperature.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 
 //Temperature calculator found in Codecademy 'Learn C: Lesson 2'
-//Things to add:
-//1. Prompt user for either Fahrenheit or Celsius
-//2. Give temperature in both the other 
+//I'm maintaining this as the "original copy". Any updates/improvements will be made to temperature_mine.cpp
 
 int main() {
   
