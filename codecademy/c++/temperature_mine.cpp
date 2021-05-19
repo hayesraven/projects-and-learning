6cc573b678224ff92fb35a00d996a95412fea5b8
thecrimsonthreat <hayesraven@gmail.com>
2021-05-19 10:54:35 -0400

    Create temperature_mine.cpp
    Modified version of temperature_mine.cpp, designed to capture updates as I learn more

---
+++ codecademy/c++/temperature_mine.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+//Temperature calculator found in Codecademy 'Learn C: Lesson 2'
+//Things to add:
+//1. Prompt user for either Fahrenheit or Celsius
+//2. Give temperature in both the other 
+
+int main() {
+  
+  // Declare variables
+  
+  double tempf, tempc;
+  int option;
+  
+  // Ask the user
+  
+  std::cout << "Choose what calculation you'd like to run today (enter 1 or 2): \n";
+  std::cout << "(1) Fahrenheit to Celsius \n";
+  std::cout << "(2) Celsius to Fahrenheit \n";
+  std::cin >> option;
+  
+  if (option == 1) {
+	
+	std::cout << "Enter the temperature in Fahrenheit: ";
+	std::cin >> tempf;
+
+	tempc = (tempf - 32) / 1.8;
+  
+	std::cout << "The temp is " << tempc << " degrees Celsius.\n";
+	
+	else
+		
+		std::cout << "Enter the temperature in Celsius: ";
+		std::cin >> tempf;
+
+		tempf = (tempc * (9 / 5)) + 32;
+  
+		std::cout << "The temp is " << tempc << " degrees Fahrenheit.\n";
+		
+	}
+  
+}
